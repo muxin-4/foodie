7e6c618aabde8373f110ceaae11c458e41253cdf
guanyu.wang <guanyu.wang@mobilemd.cn>
2019-12-31 14:59:25 +0800

    feat: cookie 与 session

--- foodie-api/src/main/java/com/muxin/controller/HelloController.java
+++ foodie-api/src/main/java/com/muxin/controller/HelloController.java
@@ -3,6 +3,9 @@ package com.muxin.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 /**
  * @program: foodie
  * @description:
@@ -15,5 +18,15 @@ public class HelloController {
     @GetMapping("/hello")
     public Object hello() {
         return "Hello World~";
+    }
+
+    @GetMapping("setSession")
+    public Object setSession(HttpServletRequest request) {
+        HttpSession session = request.getSession();
+        session.setAttribute("userInfo", "new user");
+        session.setMaxInactiveInterval(3600);
+        session.getAttribute("userInfo");
+        session.removeAttribute("user");
+        return "ok";
     }
 }
